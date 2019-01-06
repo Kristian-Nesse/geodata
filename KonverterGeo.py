6d77bef69fc37dc82dd068087733e3c99297b26b
SadSuzio <sondre01@live.no>
2019-01-06 21:16:03 +0100

    Endringer på folium og KonverterinGeo

---
+++ KonverterGeo.py
@@ -0,0 +1,71 @@
+import pandas as pd
+import numpy as np
+import utm
+import json
+
+#Tenke på og senere konvertere til Json også
+
+def konvertereFraUTM(filename):
+    #leser inn fil der xy-koordinater er på UTM32 format
+    fil16=pd.read_fwf(filename,header=None)
+    x=fil16[0]
+    z=np.array(fil16[2])
+    xy=fil16.iloc[:,0:2]
+    XY=xy.values.tolist()
+    #print(x.__len__())
+    #print(z.__len__())
+    hei=[]
+    for i in range(x.__len__()):
+        hei.append(utm.to_latlon(XY[i][1],XY[i][0],32,'U'))
+    X=[]
+    Y=[]
+    for i in range(x.__len__()):
+        X.append(hei[i][0])
+        Y.append(hei[i][1])
+    #Konverterer til numpy array
+    xx=np.array(X)
+    yy=np.array(Y)
+    Ferdig=[]
+    for i in range(x.__len__()):
+        Ferdig.append([xx[i],yy[i],z[i]])
+
+    
+    
+    
+    return hei,Ferdig
+
+#Eksempel
+
+#Konvertere fil til Json
+
+#MERK syntax for å kunn aksessere den første returnerte verdien fra metoden 
+
+J=konvertereFraUTM('16.asc')[0]
+print(J)
+
+with open('test.json', 'w') as outfile:
+        json.dump(J, outfile,indent=2)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- kartfolium.py
+++ kartfolium.py
@@ -1,13 +1,21 @@
-
+from KonverterGeo import konvertereFraUTM
 import folium
 import json
 import os
+import pandas as pd
+import utm
+
+
+
+
+
+
 
 # Create map object
 m = folium.Map(location=[58.923263, 5.648249], zoom_start=12)
 
 # Global tooltip
-tooltip = 'Click For More Info'
+tooltip = 'Klikk for mer info'
 
 logoIcon = folium.features.CustomIcon('viking.jpg', icon_size=(50, 50))
 
@@ -15,10 +23,12 @@ logoIcon = folium.features.CustomIcon('viking.jpg', icon_size=(50, 50))
 
 
 # Geojson Data
-
+vis = os.path.join('data', 'firkant.json')
 
 
 #Setter opp noko markører
+
+
 
 folium.Marker([58.925263, 5.668249],'<strong>Location one</strong>',tooltip).add_to(m)
 
@@ -39,20 +49,26 @@ folium.Marker([58.922263, 5.658249],
               popup='<strong>Location Five</strong>',
               tooltip=tooltip,
               icon=logoIcon).add_to(m),
-folium.Marker([58.912263, 5.658249],
-              popup='<strong>Location Five</strong>',
-              tooltip=tooltip,
-              icon=logoIcon).add_to(m)
+folium.Marker([58.9278263, 5.657249],
+              popup=folium.Popup(max_width=450).add_child(folium.Vega(json.load(open('vis.json')), width=450, height=250)),
+              icon=folium.Icon(color='orange', icon='leaf')).add_to(m)
 
-# sirkel markør
-folium.CircleMarker(
-    location=[58.931104,5.638220],
-    radius=25,
-    popup='Bakholdsangrep',
-    color='#428bca',
-    fill=True,
-    fill_color='#428bca'
-).add_to(m)
+
+# Circle marker
+#folium.CircleMarker(
+    #location=[58.92263, 5.658249],
+   # radius=50,
+   # popup='My Birthplace',
+  #  color='#428bca',
+ #   fill=True,
+ #   fill_color='#428bca'
+#).add_to(m)
+
+#m.choropleth(geo_data='firkant.json',line_color=None)
+
+folium.GeoJson('firkant.json', name='Harfsfjord').add_to(m)
+
+
 
 # Geojson overlay
 #folium.GeoJson('hafs.json', name='Harfsfjord').add_to(m)
