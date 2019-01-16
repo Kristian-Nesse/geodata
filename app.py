bf85e6e15aadfd352d80b7fcb8d9d6b8fb259f78
Kristian-Nesse <Kri.nesse@stud.uis.no>
2019-01-16 12:52:38 +0100

    Gjort endringer på contour plot

--- app.py
+++ app.py
@@ -35,6 +35,7 @@ def zoom():
         xstart=request.form.get('xcoord')
         xslutt=request.form.get('xcoord1')
         zoom=int(request.form.get('zoom'))
+        print(zoom)
         #Definerer variablene som blir sendt fra html siden
         if xstart==None:
             return nyliste
@@ -60,6 +61,8 @@ def zoom():
                     nyliste['x'].append(liste['xcoord'][i])
                     nyliste['y'].append(liste['ycoord'][i])
                     #Finner de nødvendige punktene i listen etter start og slutt verdiene til xstart1
+            print(nyliste)
+            print(len(nyliste['x']))
     return jsonify(nyliste)
 #Returnerer listen i ett json format slik at det er mulig for javascript å lese listen rett
 @app.route('/firstzoom',methods=['GET','POST'])
@@ -86,32 +89,42 @@ def kart():
     #Laster kartet til Sondre
     return render_template("kart2.html")
 
-@app.route('/tred',methods=['GET','POST'])
-def tred():
-    fil16=pd.read_fwf("txtfiler/16.asc",header=None)
-    print(fil16)
-    x=fil16[0]
-    y=fil16[1]
-    z=fil16[2]
-    ex=[]
-    ey=[]
-    ez=[]
-    for i in range(x.__len__()):
-      if i % 20== 0 :
-        ex.append(x[i])
-        ey.append(y[i])
-        ez.append(z[i])
-    print(ex.__len__())
-    xmin=min(ex)
-    xmax=max(ex)
-    ymin=min(ey)
-    ymax=max(ey)
-    xi = np.linspace(min(ex), max(ex))
-    yi = np.linspace(min(ey), max(ey))
+@app.route('/treD',methods=['GET','POST'])
+def treD():
+    liste=getlist()
+    nyliste={}
+    nyliste['x']=[]
+    nyliste['y']=[]
+    nyliste['z']=[]
+    xstart=request.form.get('xcoord')
+    xslutt=request.form.get('xcoord1')
+    zoom=int(request.form.get('zoom'))
+
+    if xstart>xslutt:
+                #Brukes til å definere indexen til start og sluttpunktet som er oppgitt
+             xstart1=liste['xcoord'].index(min(liste['xcoord'], key=lambda x:abs(x-round(float(xstart),2))))
+             xslutt1=liste['xcoord'].index(min(liste['xcoord'], key=lambda x:abs(x-round(float(xslutt),2))))
+    if xstart<xslutt:
+                #Er her i tilfelle zoomen er dratt motsatt vei og det første punket er høyere enn det andre
+             xstart1=liste['xcoord'].index(min(liste['xcoord'], key=lambda x:abs(x-round(float(xslutt),2))))
+             xslutt1=liste['xcoord'].index(min(liste['xcoord'], key=lambda x:abs(x-round(float(xstart),2))))
+
+    print(zoom/5)
+    
+    for i in range(int(xstart1),int(xslutt1)):
+      if i % zoom/5== 0 :
+          nyliste['z'].append(liste['zcoord'][i])
+          nyliste['x'].append(liste['xcoord'][i])
+          nyliste['y'].append(liste['ycoord'][i])
+    xmin=min(nyliste['x'])
+    xmax=max(nyliste['x'])
+    ymin=min(nyliste['y'])
+    ymax=max(nyliste['y'])
+    xi = np.linspace(xmin, xmax)
+    yi = np.linspace(ymin, ymax)
     X, Y = np.meshgrid(xi, yi)
-    spline = sp.interpolate.Rbf(ex,ey,ez,function='thin-plate')
+    spline = sp.interpolate.Rbf(nyliste['x'],nyliste['y'],nyliste['z'],function='thin-plate')
     Ze = spline(X,Y)
-    print(Ze)
     Ze2=Ze.tolist()
     return jsonify(Ze2)
 
